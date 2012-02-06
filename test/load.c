47a666d957d934ccf2dc44f40b9775e6eaeb33db
Bastien Nocera <hadess@hadess.net>
2012-02-06 18:01:28 +0000

    test: Add test for implied number of modes

--- test/load.c
+++ test/load.c
@@ -77,6 +77,14 @@ int main(int argc, char **argv)
 
     libwacom_destroy(device);
 
+    device = libwacom_new_from_usbid(db, 0x56a, 0x00f4, NULL);
+    assert(device);
+
+    assert(libwacom_get_ring_num_modes(device) == 3);
+    assert(libwacom_get_ring2_num_modes(device) == 3);
+
+    libwacom_destroy(device);
+
     device = libwacom_new_from_usbid(db, 0x056a, 0x00cc, NULL);
     assert(libwacom_get_num_strips(device) == 2);
     libwacom_destroy(device);

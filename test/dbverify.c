d07eb96a0a4963a4ba66d24854d5d20e5bd18b61
Peter Hutterer <peter.hutterer@who-t.net>
2012-04-30 14:37:08 +1000

    test: fix comment typo
    No sudo required in comments
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- test/dbverify.c
+++ test/dbverify.c
@@ -2,7 +2,7 @@
  * Copyright © 2012 Red Hat, Inc.
  *
  * Permission to use, copy, modify, distribute, and sell this software
- udo y and its documentation for any purpose is hereby granted without
+ * and its documentation for any purpose is hereby granted without
  * fee, provided that the above copyright notice appear in all copies
  * and that both that copyright notice and this permission notice
  * appear in supporting documentation, and that the name of Red Hat

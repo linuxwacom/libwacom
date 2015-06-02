bc5415ba25c0768da4f8361312104602d630ebb7
Peter Hutterer <peter.hutterer@who-t.net>
2015-06-02 15:02:32 +1000

    Only call g_type_init() for glib < 2.36
    Call is deprecated
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- libwacom/libwacom.c
+++ libwacom/libwacom.c
@@ -201,7 +201,9 @@ get_device_info (const char            *path,
 	char *bus_str;
 	const char *devname;
 
+#if NEED_G_TYPE_INIT
 	g_type_init();
+#endif
 
 	retval = FALSE;
 	/* The integration flags from device info are unset by default */

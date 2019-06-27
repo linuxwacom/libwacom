ef56aeb90d6c438160a5fb08ea5500499b39b9b6
Peter Hutterer <peter.hutterer@who-t.net>
2019-06-28 08:40:33 +1000

    data: move the git and svg checks to a separate scripts
    Easier to test, comment, write, and review when it's in a separate file.
    
    The automake integration is a bit odd. We can't pass arguments to scripts but
    the only sane way for that script is to take the top_srcdir as argument
    (otherwise it'd have to be a configure.in script). We can't do that, but we
    can set it in the environment for the tests so that's the next best thing.
    
    This also drops the custom lookups for git, grep and sed. Just make sure it's
    in your PATH. Where git ls-files fails, the test is skipped which should make
    anyone curious. This also covers the case where git is not available, so
    that's not a hard failure.
    
    For Travis: any test case failures are in the test-log files now, so let's
    just cat those so they show up in the output.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ data/check-files-in-git.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Usage: check-files-in-git.sh /path/to/libwacom/
+
+if [ -z "$top_srcdir" ]; then
+    top_srcdir="$1"
+fi
+if [ -z "$top_srcdir" ]; then
+    echo "Usage: `basename $0` /path/to/libwacom"
+    exit 1
+fi
+
+export GIT_DIR="$top_srcdir/.git";
+if ! git ls-files >& /dev/null; then
+    echo "Not a git tree. Skipping"
+    exit 77
+fi
+
+pushd "$top_srcdir" > /dev/null
+for file in data/*.tablet data/*.stylus data/layouts/*.svg; do
+    git ls-files --error-unmatch "$file" &> /dev/null || (
+        echo "ERROR: File $file is not in git" && test);
+    rc="$(($rc + $?))";
+done
+popd > /dev/null
+exit $rc

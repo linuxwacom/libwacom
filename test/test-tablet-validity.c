d1d952e8790921ffe72c894b343f8ceed89c03f0
Peter Hutterer <peter.hutterer@who-t.net>
2019-07-03 12:00:25 +1000

    test: switch the 'tablet-validity' test to use the glib test suite
    And add a new separate test for just the stylus validity tests.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- test/test-tablet-validity.c
+++ test/test-tablet-validity.c
@@ -29,6 +29,7 @@
 #endif
 
 #define _GNU_SOURCE
+#include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,31 +38,34 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "libwacom.h"
-#include <assert.h>
 #include <unistd.h>
 
 typedef int (*NumModesFn) (const WacomDevice *device);
 
-static int buttons_have_direction (WacomDevice *device)
+static gboolean
+buttons_have_direction(WacomDevice *device)
 {
 	char               button;
 	int                num_buttons;
 
 	num_buttons = libwacom_get_num_buttons (device);
 	if (num_buttons == 0)
-		return 1;
+		return TRUE;
 
 	for (button = 'A'; button < 'A' + num_buttons; button++) {
 		WacomButtonFlags  flags;
 		flags = libwacom_get_button_flag(device, button);
 		if (!(flags & WACOM_BUTTON_DIRECTION))
-			return 0;
+			return FALSE;
 	}
 
-	return 1;
+	return TRUE;
 }
 
-static int match_mode_switch (WacomDevice *device, NumModesFn get_num_modes, WacomButtonFlags flag)
+static gboolean
+match_mode_switch(WacomDevice *device,
+		  NumModesFn get_num_modes,
+		  WacomButtonFlags flag)
 {
 	char               button;
 	int                num_buttons;
@@ -85,34 +89,20 @@ static int match_mode_switch (WacomDevice *device, NumModesFn get_num_modes, Wac
 	 * number of modes must match the number of mode-switch buttons.
 	 */
 	if (num_switches > 1 && num_modes != num_switches)
-		return 0;
+		return FALSE;
 
 	/*
 	 * If we have more than one mode, then we should find at least
 	 * one mode-switch button.
 	 */
 	if (num_modes > 1 && num_switches == 0)
-		return 0;
+		return FALSE;
 
-	return 1;
+	return TRUE;
 }
 
-static int eraser_is_present(WacomDeviceDatabase *db, const int *styli, int nstyli, WacomStylusType type)
-{
-	int i;
-
-	for (i = 0; i < nstyli; i++) {
-		const WacomStylus *stylus;
-		stylus = libwacom_stylus_get_for_id (db, styli[i]);
-		if (libwacom_stylus_is_eraser (stylus) &&
-		    libwacom_stylus_get_type (stylus) == type)
-			return 1;
-	}
-
-	return 0;
-}
-
-static int tablet_has_lr_buttons(WacomDevice *device)
+static gboolean
+tablet_has_lr_buttons(WacomDevice *device)
 {
 	int nleft = 0;
 	int nright = 0;
@@ -130,134 +120,223 @@ static int tablet_has_lr_buttons(WacomDevice *device)
 	}
 
 	if (nleft > 0 || nright > 0)
-		return 1;
+		return TRUE;
 
-	return 0;
+	return FALSE;
 }
 
-static void verify_tablet(WacomDeviceDatabase *db, WacomDevice *device)
+static void
+test_class(gconstpointer data)
 {
-	const char *name;
-	const int *styli;
-	int nstyli, i;
+	WacomDevice *device = (WacomDevice*)data;
 
-	name = libwacom_get_name(device);
-	if (strcmp(name, "Generic") == 0)
-		return;
-
-	printf("Verifying tablet %s\n", name);
-	assert(libwacom_get_class(device) != WCLASS_UNKNOWN);
-	assert(name && strlen(name) > 0);
-	if (libwacom_get_bustype(device) != WBUSTYPE_SERIAL) {
-		assert(libwacom_get_vendor_id(device) > 0);
-		assert(libwacom_get_product_id(device) > 0);
-	}
-	assert(libwacom_get_match(device) != NULL);
-	assert(libwacom_get_matches(device) != NULL);
-
-	/* ISDv4 are built-in, they may be of varying size */
-	if (libwacom_get_class(device) != WCLASS_ISDV4 &&
-	    libwacom_get_class(device) != WCLASS_REMOTE) {
-		assert(libwacom_get_width(device) > 0);
-		assert(libwacom_get_height(device) > 0);
-	}
-	assert(libwacom_get_num_buttons(device) >= 0);
-
-	styli = libwacom_get_supported_styli(device, &nstyli);
-
-	if (libwacom_has_stylus(device)) {
-		assert(styli != NULL);
-		assert(nstyli >= 1);
-	}
-
-	switch(libwacom_get_class(device)) {
+	switch (libwacom_get_class(device)) {
 		case WCLASS_BAMBOO:
 		case WCLASS_ISDV4:
 		case WCLASS_PEN_DISPLAYS:
 		case WCLASS_GRAPHIRE:
 		case WCLASS_REMOTE:
-			break;
 		case WCLASS_INTUOS:
 		case WCLASS_INTUOS2:
 		case WCLASS_INTUOS3:
 		case WCLASS_INTUOS4:
 		case WCLASS_INTUOS5:
 		case WCLASS_CINTIQ:
-			{
-				int i;
-				for (i = 0; i < nstyli; i++) {
-					assert(styli[i] != WACOM_STYLUS_FALLBACK_ID);
-					assert(styli[i] != WACOM_ERASER_FALLBACK_ID);
-				}
-			}
 			break;
 		default:
-			abort(); /* don't get here */
+			g_test_fail();
+			break;
 	}
-
-	for (i = 0; i < nstyli; i++) {
-		const WacomStylus *stylus;
-		const char *stylus_name;
-		WacomAxisTypeFlags axes;
-
-		stylus = libwacom_stylus_get_for_id (db, styli[i]);
-		assert(stylus);
-		stylus_name = libwacom_stylus_get_name (stylus);
-		assert(stylus_name);
-		if (libwacom_stylus_has_eraser (stylus)) {
-			WacomStylusType type;
-			type = libwacom_stylus_get_type (stylus);
-			assert(eraser_is_present (db, styli, nstyli, type));
-		}
-
-		if (libwacom_stylus_get_type (stylus) == WSTYLUS_PUCK) {
-			int has_wheel = libwacom_stylus_has_wheel (stylus);
-			int has_lens = libwacom_stylus_has_lens (stylus);
-			/* 4D mouse is the only one with neither, everything
-			 * else has either wheel or lens */
-			if (styli[i] == 0x94) {
-				assert (!has_wheel);
-				assert (!has_lens);
-			} else {
-				assert (has_wheel != has_lens);
-			}
-		}
-
-		if (libwacom_stylus_is_eraser (stylus))
-			assert (libwacom_stylus_get_num_buttons (stylus) > 0);
-
-		axes = libwacom_stylus_get_axes (stylus);
-		if (libwacom_stylus_get_type (stylus) == WSTYLUS_PUCK) {
-			assert((axes & WACOM_AXIS_TYPE_PRESSURE) == 0);
-		} else if ((styli[i] != 0xffffd) && (styli[i] != 0x8e2) && (styli[i] != 0x862)) {
-			assert(axes & WACOM_AXIS_TYPE_TILT);
-			assert(axes & WACOM_AXIS_TYPE_PRESSURE);
-			assert(axes & WACOM_AXIS_TYPE_DISTANCE);
-		}
-	}
-	assert(libwacom_get_ring_num_modes(device) >= 0);
-	assert(libwacom_get_ring2_num_modes(device) >= 0);
-	assert(libwacom_get_num_strips(device) >= 0);
-	assert(libwacom_get_strips_num_modes(device) >= 0);
-	assert(libwacom_get_bustype(device) != WBUSTYPE_UNKNOWN);
-	assert(buttons_have_direction(device) > 0);
-	if (libwacom_has_ring(device))
-		assert(match_mode_switch (device, libwacom_get_ring_num_modes, WACOM_BUTTON_RING_MODESWITCH));
-	if (libwacom_has_ring2(device))
-		assert(match_mode_switch (device, libwacom_get_ring2_num_modes, WACOM_BUTTON_RING2_MODESWITCH));
-	if (libwacom_get_num_strips(device) > 1)
-		assert(match_mode_switch (device, libwacom_get_strips_num_modes, WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH));
-	if (libwacom_get_num_strips(device) > 0)
-		assert(match_mode_switch (device, libwacom_get_strips_num_modes, WACOM_BUTTON_TOUCHSTRIP_MODESWITCH));
-
-	if (libwacom_is_reversible(device) && libwacom_get_num_buttons(device) > 0)
-		assert(tablet_has_lr_buttons(device));
 }
 
-int main(void)
+static void
+test_name(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	g_assert_nonnull(libwacom_get_name(device));
+	g_assert_cmpstr(libwacom_get_name(device), !=, "");
+}
+
+static void
+test_vidpid(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	switch (libwacom_get_bustype(device)) {
+		case WBUSTYPE_SERIAL:
+			g_assert_cmpint(libwacom_get_vendor_id(device), ==, 0);
+			g_assert_cmpint(libwacom_get_product_id(device), ==, 0);
+			break;
+		case WBUSTYPE_USB:
+		case WBUSTYPE_BLUETOOTH:
+		case WBUSTYPE_I2C:
+			g_assert_cmpint(libwacom_get_vendor_id(device), >, 0);
+			g_assert_cmpint(libwacom_get_product_id(device), >, 0);
+			break;
+		case WBUSTYPE_UNKNOWN:
+		default:
+			g_test_fail();
+			break;
+	}
+}
+
+static void
+test_matches(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	g_assert_nonnull(libwacom_get_match(device));
+	g_assert_nonnull(libwacom_get_matches(device));
+}
+
+static void
+test_dimensions(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	g_assert_cmpint(libwacom_get_width(device), >, 0);
+	g_assert_cmpint(libwacom_get_height(device), >, 0);
+}
+
+static void
+test_buttons(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	g_assert_cmpint(libwacom_get_num_buttons(device), >=, 0);
+	g_assert_true(buttons_have_direction(device));
+
+	if (libwacom_is_reversible(device) && libwacom_get_num_buttons(device) > 0)
+		g_assert_true(tablet_has_lr_buttons(device));
+}
+
+static void
+test_styli(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+	int nstyli;
+	const int *styli = libwacom_get_supported_styli(device, &nstyli);
+
+	g_assert_cmpint(nstyli, >, 0);
+	g_assert_nonnull(styli);
+}
+
+static void
+test_realstylus(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+	int nstyli;
+	const int *styli = libwacom_get_supported_styli(device, &nstyli);
+
+	for (int i = 0; i < nstyli; i++) {
+		g_assert_cmpint(styli[i], !=, WACOM_STYLUS_FALLBACK_ID);
+		g_assert_cmpint(styli[i], !=, WACOM_ERASER_FALLBACK_ID);
+	}
+}
+
+static void
+test_rings(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	g_assert_cmpint(libwacom_get_ring_num_modes(device), >=, 0);
+	g_assert_cmpint(libwacom_get_ring2_num_modes(device), >=, 0);
+
+	if (libwacom_has_ring(device))
+		g_assert_true(match_mode_switch(device,
+						libwacom_get_ring_num_modes,
+						WACOM_BUTTON_RING_MODESWITCH));
+	if (libwacom_has_ring2(device))
+		g_assert_true(match_mode_switch(device,
+						libwacom_get_ring2_num_modes,
+						WACOM_BUTTON_RING2_MODESWITCH));
+}
+
+static void
+test_strips(gconstpointer data)
+{
+	WacomDevice *device = (WacomDevice*)data;
+
+	g_assert_cmpint(libwacom_get_num_strips(device), >=, 0);
+	g_assert_cmpint(libwacom_get_strips_num_modes(device), >=, 0);
+
+	if (libwacom_get_num_strips(device) > 0)
+		g_assert_true(match_mode_switch(device,
+						libwacom_get_strips_num_modes,
+						WACOM_BUTTON_TOUCHSTRIP_MODESWITCH));
+}
+
+/* Wrapper function to make adding tests simpler. g_test requires
+ * a unique test case name so we assemble that from the test function and
+ * the tablet data.
+ */
+static inline void
+_add_test(WacomDevice *device, GTestDataFunc func, const char *funcname)
+{
+	char buf[128];
+	static int count; /* guarantee unique test case names */
+	const char *prefix;
+
+	/* tests must be test_foobar */
+	g_assert(strncmp(funcname, "test_", 5) == 0);
+	prefix = &funcname[5];
+
+	snprintf(buf, 128, "/tablet/%s/%03d/%04x:%04x-%s",
+		 prefix,
+		 ++count,
+		 libwacom_get_vendor_id(device),
+		 libwacom_get_product_id(device),
+		 libwacom_get_name(device));
+	g_test_add_data_func(buf, device, func);
+}
+#define add_test(device_, func_) \
+	_add_test(device_, func_, #func_)
+
+static void setup_tests(WacomDevice *device)
+{
+	const char *name;
+
+	name = libwacom_get_name(device);
+	if (strcmp(name, "Generic") == 0)
+		return;
+
+	add_test(device, test_class);
+	add_test(device, test_name);
+	add_test(device, test_vidpid);
+	add_test(device, test_matches);
+	add_test(device, test_buttons);
+	add_test(device, test_styli);
+	add_test(device, test_rings);
+	add_test(device, test_strips);
+
+	/* ISDv4 are built-in, they may be of varying size */
+	if (libwacom_get_class(device) != WCLASS_ISDV4 &&
+	    libwacom_get_class(device) != WCLASS_REMOTE)
+		add_test(device, test_dimensions);
+
+	/* FIXME: we force the generic pen for these, should add a test */
+	if (libwacom_has_stylus(device))
+		add_test(device, test_styli);
+
+	switch (libwacom_get_class(device)) {
+		case WCLASS_INTUOS:
+		case WCLASS_INTUOS2:
+		case WCLASS_INTUOS3:
+		case WCLASS_INTUOS4:
+		case WCLASS_INTUOS5:
+		case WCLASS_CINTIQ:
+			add_test(device, test_realstylus);
+			break;
+		default:
+			break;
+	}
+}
+
+static WacomDeviceDatabase *
+load_database(void)
 {
 	WacomDeviceDatabase *db;
-	WacomDevice **device, **devices;
 	const char *datadir;
 
 	datadir = getenv("LIBWACOM_DATA_DIR");
@@ -267,20 +346,35 @@ int main(void)
 	db = libwacom_database_new_for_path(datadir);
 	if (!db)
 		printf("Failed to load data from %s", datadir);
-	assert(db);
+
+	g_assert(db);
+	return db;
+}
+
+int main(int argc, char **argv)
+{
+	WacomDeviceDatabase *db;
+	WacomDevice **devices;
+	int rc;
+
+	g_test_init(&argc, &argv, NULL);
+	g_test_set_nonfatal_assertions();
+
+	db = load_database();
 
 	devices = libwacom_list_devices_from_database(db, NULL);
-	assert(devices);
-	assert(*devices);
+	g_assert(devices);
+	g_assert(*devices);
 
-	for (device = devices; *device; device++)
-		verify_tablet(db, *device);
+	for (WacomDevice **device = devices; *device; device++)
+		setup_tests(*device);
 
-	libwacom_database_destroy (db);
+	rc = g_test_run();
 
+	libwacom_database_destroy(db);
 	free(devices);
 
-	return 0;
+	return rc;
 }
 
 /* vim: set noexpandtab tabstop=8 shiftwidth=8: */

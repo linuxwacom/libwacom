89dcaecb5e0beb609e4855cc36a43a0413d7eaa5
Peter Hutterer <peter.hutterer@who-t.net>
2019-07-04 11:10:41 +1000

    Add a script to run a full test locally
    Runs the test suite, in valgrind, with ubsan, with asan and finally distcheck.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ run-full-test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+
+date=`date +"%Y-%m-%d-%H.%M.%S"`
+builddir="build.$date"
+
+echo "####################################### running test suite"
+meson $builddir
+ninja -C $builddir test
+
+echo "####################################### running valgrind"
+pushd $builddir > /dev/null
+meson test --setup=valgrind --suite=valgrind
+popd > /dev/null
+
+echo "####################################### running ubsan"
+meson configure $builddir  -Db_sanitize=undefined
+ninja -C $builddir test
+
+echo "####################################### running asan"
+meson configure $builddir  -Db_sanitize=address
+ninja -C $builddir test
+
+echo "####################################### running scan-build"
+meson configure $builddir  -Db_sanitize=none
+ninja -C $builddir scan-build
+
+echo "####################################### running make distcheck"
+
+# we can't configure with a custom sourcetree if we have run configure
+# previously. So let's only do this where we have a clean tree that we can
+# clone.
+if git diff --exit-code -s; then
+    mkdir -p "$builddir/autotools/build"
+    mkdir -p "$builddir/autotools/inst"
+    pushd "$builddir/autotools" > /dev/null
+    git clone ../.. "src"
+    pushd src > /dev/null
+    autoreconf -ivf
+    popd
+    pushd build > /dev/null
+    ../src/configure --prefix=$PWD/../inst/
+    make && make check
+    make install
+    make distcheck
+    popd > /dev/null
+    popd > /dev/null
+else
+    echo "local changes present, skipping make distcheck"
+fi
+
+echo "######## Success. Removing builddir #########"
+rm -rf "$buildir"

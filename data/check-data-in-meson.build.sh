c9fadf756376603587e09ada3561eec72a9461f1
Peter Hutterer <peter.hutterer@who-t.net>
2020-03-27 13:22:15 +1000

    meson: use install_subdir() to install the data files
    Somewhat of an improvement over having to name every tablet file in the
    meson.build file.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- data/check-data-in-meson.build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-pushd "$1" > /dev/null
-diff -u1 <(grep -o 'data/.*\.tablet' meson.build) <(printf '%s\n' data/*.tablet | sort -V)
-diff -u1 <(grep -o 'data/.*\.stylus' meson.build) <(printf '%s\n' data/*.stylus | sort -V)
-diff -u1 <(grep -o 'data/layouts/.*\.svg' meson.build) <(printf '%s\n' data/layouts/*.svg | sort -V)
-popd > /dev/null

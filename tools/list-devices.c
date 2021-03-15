e557225a8df0e23646e828c788d8d63678912dff
Peter Hutterer <peter.hutterer@who-t.net>
2021-03-16 07:04:42 +1000

    tools: invert an if condition to use continue instead
    No functional changes, just prep work.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- tools/list-devices.c
+++ tools/list-devices.c
@@ -42,10 +42,11 @@ static void print_device_info (WacomDevice *device, WacomBusType bus_type_filter
 
 	for (match = libwacom_get_matches(device); *match; match++) {
 		WacomBusType type = libwacom_match_get_bustype(*match);
-		if (type == bus_type_filter) {
-			libwacom_print_device_description(STDOUT_FILENO, device);
-			dprintf(STDOUT_FILENO, "---------------------------------------------------------------\n");
-		}
+		if (type != bus_type_filter)
+			continue;
+
+		libwacom_print_device_description(STDOUT_FILENO, device);
+		dprintf(STDOUT_FILENO, "---------------------------------------------------------------\n");
 	}
 }
 

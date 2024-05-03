d745bf7dcc19207b08285098f76339b9edbc129c
Peter Hutterer <peter.hutterer@who-t.net>
2024-05-03 10:41:40 +1000

    test: add a pytest wrapper for libwacom
    This wraps libwacom into python ctypes interface, including a wrapper
    class that makes interacting with it a bit more pythonic. Most of this
    code is taken from libei's oeffis pytests.

---
+++ test/conftest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# This file is formatted with ruff format
+#
+
+from pathlib import Path
+import logging
+import os
+import pytest
+
+from . import WacomDatabase
+
+logger = logging.getLogger(__name__)
+
+
+def load_test_db() -> WacomDatabase:
+    if any(os.environ.get(v) for v in ["ASAN_OPTIONS", "UBSAN_OPTIONS"]):
+        pytest.skip("ASAN/UBSAN not supported in this module")
+
+    try:
+        dbpath = os.environ.get("MESON_SOURCE_ROOT")
+        if dbpath is None and (Path.cwd() / "meson.build").exists():
+            dbpath = Path.cwd()
+            logger.info(f"Defaulting to MESON_SOURCE_ROOT={dbpath}")
+        return WacomDatabase(path=Path(dbpath) / "data" if dbpath else None)
+    except AttributeError as e:
+        pytest.exit(f"Failed to initialize and wrap libwacom.so: {e}. You may need to set LD_LIBRARY_PATH and optionally MESON_SOURCE_ROOT")
+
+
+@pytest.fixture()
+def db():
+    return load_test_db()

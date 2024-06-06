0b37ee5b84a4d4c58bcb5198877a729a5194a617
Peter Hutterer <peter.hutterer@who-t.net>
2024-06-06 11:34:44 +1000

    CI: handle python3.12's EXTERNALLY-MANAGED file
    If that file is present, pass --break-system-packages to pip.

--- .github/actions/pkginstall/install-pip.sh
+++ .github/actions/pkginstall/install-pip.sh
@@ -5,5 +5,10 @@ if [[ -z "$@" ]]; then
 	exit
 fi
 
-python -m pip install --upgrade pip
-python -m pip install --upgrade "$@"
+FLAGS=""
+if ls /usr/lib/python3.*/EXTERNALLY-MANAGED >/dev/null 2>&1; then
+	FLAGS="--break-system-packages"
+fi
+
+python -m pip install --upgrade $FLAGS pip
+python -m pip install --upgrade $FLAGS "$@"
